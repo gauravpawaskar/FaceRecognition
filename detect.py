d31b86005c9ee337db54f827d1f4060f7aa7baf0
gauravpawaskar <pawaskar.gaurav@gmail.com>
2018-12-30 20:55:48 -0800

    attendance code

--- detect.py
+++ detect.py
@@ -10,86 +10,99 @@ import requests
 from PIL import Image, ImageTk
 import json
 
-class Home:
-    def __init__(self, master):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.labelClass = tk.Label(self.frame, text="Class")
-        self.labelClass.grid(row=1,column=1)
-        self.textClass = tk.Text(self.frame, height=1, width=10)
-        self.textClass.grid(row=1,column=2)
-        self.button1 = tk.Button(self.frame, text = 'Start', width = 25, command = self.new_window)
-        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-        self.button1.grid(row=2,column=1, columnspan=2)
-        self.quitButton.grid(row=3,column=1, columnspan=2)
-        self.frame.grid(row=4,column=1)
+classId = 0
 
-    def new_window(self):
-        self.newWindow = tk.Toplevel(self.master)
-        self.app = Detect(self.newWindow, self.textClass.get("1.0","end-1c"))
+class Home:
+  def __init__(self, master):
+    self.master = master
+    self.frame = tk.Frame(self.master)
+    self.labelClass = tk.Label(self.frame, text="Class")
+    self.labelClass.grid(row=1,column=1)
+    self.textClass = tk.Text(self.frame, height=1, width=10)
+    self.textClass.grid(row=1,column=2)
+    self.button1 = tk.Button(self.frame, text = 'Start', width = 25, command = self.new_window)
+    self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+    self.button1.grid(row=2,column=1, columnspan=2)
+    self.quitButton.grid(row=3,column=1, columnspan=2)
+    self.frame.grid(row=4,column=1)
+
+  def new_window(self):
+    self.newWindow = tk.Toplevel(self.master)
+    self.app = Detect(self.newWindow, self.textClass.get("1.0","end-1c"))
         
-    def close_windows(self):
-        self.master.destroy()
+  def close_windows(self):
+    self.master.destroy()
 
 class Detect:
-    def __init__(self, master, cls):
-        self.master = master
-        self.frame = tk.Frame(self.master)
-        self.master.title("Detect")
-        self.knownFaceEncode = []
-        self.FNames = []
-        self.LNames = []
-        self.Rolls = []
-        URL = "http://localhost/getclass?class="+cls
-        res = requests.get(url=URL)
-        if res.status_code == 200:
-			records = res.json()
-			for record in records:
-				self.knownFaceEncode.append(np.asarray(json.loads(record["image"])))
-				self.FNames.append(record["fname"])
-				self.LNames.append(record["lname"])
-				self.Rolls.append(record["roll"])
+  def __init__(self, master, cls):
+    global classId
+    self.master = master
+    self.frame = tk.Frame(self.master)
+    self.master.title("Detect")
+    self.knownFaceEncode = []
+    self.FNames = []
+    self.LNames = []
+    self.Rolls = []
+    classId = cls
+    URL = "http://localhost/getclass?class="+cls
+    res = requests.get(url=URL)
+    if res.status_code == 200:
+      records = res.json()
+      for record in records:
+        self.knownFaceEncode.append(np.asarray(json.loads(record["image"])))
+        self.FNames.append(record["fname"])
+        self.LNames.append(record["lname"])
+        self.Rolls.append(record["roll"])
+    
+    self.master.vs = cv2.VideoCapture(0)
+    self.master.current_image = None
+    self.panel = tk.Label(self.master)
+    self.panel.grid(row=1, column=2, columnspan=2)
+    
+    self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
+    self.quitButton.grid(row=6,column=2)
+    self.frame.grid(row=2,column=2)
+    self.video_loop()
         
-        self.master.vs = cv2.VideoCapture(0)
-        self.master.current_image = None
-        self.panel = tk.Label(self.master)
-        self.panel.grid(row=1, column=2, columnspan=2)
-        
-        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_windows)
-        self.quitButton.grid(row=6,column=2)
-        self.frame.grid(row=2,column=2)
-        self.video_loop()
-        
-    def video_loop(self):
-      ok, frame = self.master.vs.read()
-      if ok:
-        key = cv2.waitKey(1000)
-        self.cv2image = frame[:, :, ::-1]
-        self.current_image = Image.fromarray(self.cv2image)
-        imgtk = ImageTk.PhotoImage(image=self.current_image)
-        self.panel.imgtk = imgtk
-        self.panel.config(image=imgtk)
-        matchLoc = face_recognition.face_locations(self.cv2image)
-        matchEncodes = face_recognition.face_encodings(self.cv2image, matchLoc)
-        for matchEncode in matchEncodes:
-			matches = face_recognition.compare_faces(self.knownFaceEncode, matchEncode)
-			if True in matches:
-			  first_match_index = matches.index(True)
-			  print self.Rolls[first_match_index], self.FNames[first_match_index], self.LNames[first_match_index]
-			  self.knownFaceEncode = self.knownFaceEncode[:first_match_index] + self.knownFaceEncode[first_match_index+1 :]
-			  self.Rolls = self.Rolls[:first_match_index] + self.Rolls[first_match_index+1 :]
-			  self.FNames = self.FNames[:first_match_index] + self.FNames[first_match_index+1 :]
-			  self.LNames = self.LNames[:first_match_index] + self.LNames[first_match_index+1 :]
-      self.master.after(1, self.video_loop)
+  def video_loop(self):
+    global classId
+    ok, frame = self.master.vs.read()
+    if ok:
+      key = cv2.waitKey(1000)
+      self.cv2image = frame[:, :, ::-1]
+      self.current_image = Image.fromarray(self.cv2image)
+      imgtk = ImageTk.PhotoImage(image=self.current_image)
+      self.panel.imgtk = imgtk
+      self.panel.config(image=imgtk)
+      matchLoc = face_recognition.face_locations(self.cv2image)
+      matchEncodes = face_recognition.face_encodings(self.cv2image, matchLoc)
+      for matchEncode in matchEncodes:
+        matches = face_recognition.compare_faces(self.knownFaceEncode, matchEncode)
+        if True in matches:
+          first_match_index = matches.index(True)
+          URL = "http://localhost/attend"
+          headers = {"Content-Type": "application/json"}
+          data = {
+            "rn" : self.Rolls[first_match_index],
+            "class" : classId,
+          }
+          res = requests.post(url=URL, data=json.dumps(data), headers=headers)
+          if res.status_code == 200:
+            print self.Rolls[first_match_index], self.FNames[first_match_index], self.LNames[first_match_index], classId
+          self.knownFaceEncode = self.knownFaceEncode[:first_match_index] + self.knownFaceEncode[first_match_index+1 :]
+          self.Rolls = self.Rolls[:first_match_index] + self.Rolls[first_match_index+1 :]
+          self.FNames = self.FNames[:first_match_index] + self.FNames[first_match_index+1 :]
+          self.LNames = self.LNames[:first_match_index] + self.LNames[first_match_index+1 :]
+    self.master.after(1, self.video_loop)
 
-    def close_windows(self):
-      self.master.vs.release()
-      self.master.destroy()
+  def close_windows(self):
+    self.master.vs.release()
+    self.master.destroy()
 
 def main(): 
-    root = tk.Tk()
-    app = Home(root)
-    root.mainloop()
+  root = tk.Tk()
+  app = Home(root)
+  root.mainloop()
 
 if __name__ == '__main__':
-    main()
+  main()
--- server.py
+++ server.py
@@ -7,11 +7,7 @@ import json
 import face_recognition
 import numpy as np
 import os
-#import sqlite3
-#from sqlite3 import Error
 import mysql.connector
-
-#database = "student.db"
 
 db = mysql.connector.connect(user='root', password='root', host='127.0.0.1', database='student', buffered=True)
 
@@ -19,14 +15,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
+  db = getattr(g, '_database', None)
+  if db is not None:
+    db.close()
 
 @app.route("/")
 def hello():
-        #template = Template("Hello {{ name }}!")
-        return render_template("home.html")
+  return render_template("home.html")
 
 @app.route("/enroll", methods=["POST"])
 def enroll():
@@ -36,12 +31,9 @@ def enroll():
   sql = "INSERT INTO student (rn, fname, lname, image, class) VALUES (%s,%s,%s,%s,%s)"
   values = (int(req_data["roll"]), req_data["fname"], req_data["lname"], json.dumps(np.asarray(req_data["image"]).tolist()) , req_data["class"])
   print values
-  #try:
   res = cur.execute(sql, values)
   print cur.lastrowid
   db.commit()
-  #except Exception as e:
-   # print e
   return "ok"
   
 @app.route("/getclass", methods=["GET"])
@@ -50,12 +42,10 @@ def getClass():
   className = request.args.get("class").encode('ascii','ignore')
   sql = "SELECT * FROM student WHERE class = %s"
   values = (className,)
-  #print sql, values
   cur.execute(sql, values)
   sendRes = []
   rows = cur.fetchall()
   for row in rows:
-    #print row["lname"]
     data = {
 	    "roll" : row["rn"],
 	    "fname" : row["fname"],
@@ -64,6 +54,16 @@ def getClass():
 	  }
     sendRes.append(data)
   return json.dumps(sendRes)
+
+@app.route("/attend", methods=["POST"])
+def attend():
+  req_data = request.get_json()
+  cur = db.cursor()
+  sql = "INSERT INTO attend (rn, class) VALUES (%s, %s)"
+  values = (int(req_data["rn"]), req_data["class"])
+  res = cur.execute(sql, values)
+  db.commit()
+  return "ok"
 
 if __name__ == "__main__":
   app.run(debug=True, host="0.0.0.0", port=80)
